e4daf034564d64166e70d059532934f7aff35748
Mike Bell <michaelbell.va@gmail.com>
2018-12-11 17:03:19 -0500

    styling improvements

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 let prevScrollPos = window.pageYOffset;
+console.log(prevScrollPos);
 window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
-  if (prevScrollPos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
+  console.log(currentScrollPos)
+  if (currentScrollPos > 895) {
+    document.getElementById("header").style.color = "#333";
+    document.getElementById("header").style.textShadow = "0 3px 6px rgba(0,0,0,0.23), 0 3px 6px rgba(0,0,0,0.23)";
+
   } else {
-    document.getElementById("header").style.top = "-70px";
+    document.getElementById("header").style.color = "white";
+    document.getElementById("header").style.boxShadow = "none"
+
   }
   prevScrollPos = currentScrollPos;
 }
 
 // This is to control scroll animations using "ScrollReveal library".
-ScrollReveal().reveal(".h2-portfolio", { delay: 900 });
+ScrollReveal().reveal(".h2-portfolio", { delay: 500 });
 ScrollReveal().reveal(".p4-container", { delay: 900 });
 ScrollReveal().reveal(".p1-container", { delay: 900 });
 ScrollReveal().reveal(".p2-container", { delay: 900 });
