a4f8c0ff648ab4199463b6fe732b77e792230e13
Mike Bell <michaelbell.va@gmail.com>
2018-12-14 17:25:23 -0500

    switch header children positions

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-let prevScrollPos = window.pageYOffset;
-console.log(prevScrollPos);
-window.onscroll = () => {
-  let currentScrollPos = window.pageYOffset;
-  console.log(currentScrollPos)
-  if (currentScrollPos > 895) {
-    document.getElementById("header").style.color = "#333";
-    document.getElementById("header").style.textShadow = "0 3px 6px rgba(0,0,0,0.23), 0 3px 6px rgba(0,0,0,0.23)";
+// // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// let prevScrollPos = window.pageYOffset;
+// console.log(prevScrollPos);
+// window.onscroll = () => {
+//   let currentScrollPos = window.pageYOffset;
+//   console.log(currentScrollPos)
+//   if (currentScrollPos > 895) {
+//     document.getElementById("header").style.color = "#333";
+//     document.getElementById("header").style.textShadow = "0 3px 6px rgba(0,0,0,0.23), 0 3px 6px rgba(0,0,0,0.23)";
 
-  } else {
-    document.getElementById("header").style.color = "white";
-    document.getElementById("header").style.boxShadow = "none"
+//   } else {
+//     document.getElementById("header").style.color = "white";
+//     document.getElementById("header").style.boxShadow = "none"
 
-  }
-  prevScrollPos = currentScrollPos;
-}
+//   }
+//   prevScrollPos = currentScrollPos;
+// }
 
 // Open nav overlay
 const openNav = () => {
-  document.getElementById("myNav").style.width="100%";
+    document.getElementById("myNav").style.width="100%";
 }
 // Close nav overlay
 const closeNav = () => {
