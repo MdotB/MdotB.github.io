3f57c6385595c1e2518977f3f59c4c10d74df1c0
Mike Bell <michaelbell.va@gmail.com>
2019-01-30 13:26:15 -0500

    add 'explore' button on hero section

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-// // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// Reveal headerNav on scroll from top.
 let prevScrollPos = window.pageYOffset;
 console.log(prevScrollPos);
 window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
   console.log(currentScrollPos)
+
+  const headerNavLinks = document.querySelectorAll(".header-nav-links");
+  const burger = document.getElementById("burger-menu");
   if (currentScrollPos > 0) {
-    document.getElementById("header-nav").style.backgroundColor = "rgba(255,255,255,.9)";
-    document.getElementById("header-nav").style.boxShadow = "0 3px 6px rgba(0,0,0,0.23), 0 3px 6px rgba(0,0,0,0.23)";
-
+    headerNavLinks.forEach(headerLink => headerLink.style.color = "#333");
+    burger.style.color = "#333";
+    document.getElementById("header-nav")
+      .style.backgroundColor = "rgba(255,255,255,.9)";
+    document.getElementById("header-nav")
+      .style.boxShadow = "0 3px 6px rgba(0,0,0,0.23), 0 3px 6px rgba(0,0,0,0.23)";
   } else {
-    document.getElementById("header-nav").style.color = "#fff";
-    document.getElementById("header-nav").style.backgroundColor = "rgba(255,255,255,0)";
-    document.getElementById("header-nav").style.boxShadow = "none";
-
-  }
+      headerNavLinks.forEach(headerLink => headerLink.style.color = "#fff");
+      burger.style.color = "#fff";
+      document.getElementById("header-nav").style.backgroundColor = "rgba(255,255,255,0)";
+      document.getElementById("header-nav").style.boxShadow = "none";
+    }
   prevScrollPos = currentScrollPos;
 }
 
 
 
 const sideNav = document.getElementById("myNav");
-const bodyGetsPushed = document.getElementById("pushNav");
+const pushedBody = document.getElementById("pushNav");
 
 // Open nav overlay
 const openNav = () => {
   sideNav.style.width="250px";
-  bodyGetsPushed.style.marginLeft="500px";
+  pushedBody.style.marginLeft="500px";
 }
 // Close nav overlay
 const closeNav = () => {  
   sideNav.style.width="0%";
-  bodyGetsPushed.style.margin="0";
+  pushedBody.style.margin="0";
 }
 // Toggle nav
 const toggleNav = () => {
