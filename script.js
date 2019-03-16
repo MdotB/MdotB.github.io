191cc2a1fd70580a3556bea16833d447575eb317
Mike Bell <michaelbell.va@gmail.com>
2019-03-15 22:19:21 -0400

    animate burger menu and change bg color footer

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const burgerBars = document.querySelectorAll(".bar");
+const burger = document.getElementById("burger-menu");
+
 // Reveal headerNav on scroll from top.
 let prevScrollPos = window.pageYOffset;
 console.log(prevScrollPos);
@@ -6,40 +9,44 @@ window.onscroll = () => {
   console.log(currentScrollPos)
 
   const headerNavLinks = document.querySelectorAll(".header-nav-links");
-  const burger = document.getElementById("burger-menu");
+  
   if (currentScrollPos > 0) {
     headerNavLinks.forEach(headerLink => headerLink.style.color = "#333");
-    burger.style.color = "#333";
+    burgerBars.forEach(burgerBar => burgerBar.style.backgroundColor = "#333");
     document.getElementById("header-nav")
-      .style.backgroundColor = "rgba(255,255,255,.9)";
+      .style.backgroundColor = "rgba(255,255,255,1)";
     document.getElementById("header-nav")
       .style.boxShadow = "0 3px 6px rgba(0,0,0,0.23), 0 3px 6px rgba(0,0,0,0.23)";
   } else {
       headerNavLinks.forEach(headerLink => headerLink.style.color = "#fff");
-      burger.style.color = "#fff";
+      burgerBars.forEach(burgerBar => burgerBar.style.backgroundColor = "#fff");
       document.getElementById("header-nav").style.backgroundColor = "rgba(255,255,255,0)";
       document.getElementById("header-nav").style.boxShadow = "none";
     }
   prevScrollPos = currentScrollPos;
 }
 
-
+function myFunction(x) {
+  x.classList.toggle("change");
+}
 
 const overlayNav = document.getElementById("overlayNav");
-// const pushedBody = document.getElementById("pushNav");
+
 
 // Open nav overlay
 const openNav = () => {
-  overlayNav.style.height="100vh";
-  // pushedBody.style.marginTop="500px";
+  overlayNav.style.height="auto";
+  overlayNav.style.borderBottom="3px solid #000";
+  overlayNav.style.borderTop="2px solid #000";
 }
 // Close nav overlay
-const closeNav = () => {  
+const closeNav = () => {
   overlayNav.style.height="0%";
-  // pushedBody.style.margin="0";
+  overlayNav.style.border="none"
 }
 // Toggle nav
-const toggleNav = () => {
+const toggleNav = (x) => {
+  x.classList.toggle("change")
     if (overlayNav.style.height === "0%") {
       openNav()
     } else {
